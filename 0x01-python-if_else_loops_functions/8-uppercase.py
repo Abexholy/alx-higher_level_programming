319db66688a42433b99f3fb7a2a8acf90583f0a0
Abexholy <abexholy123@gmail.com>
2024-01-16 19:17:21 -0800

    pythhon

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/bin/bin/python3
+def uppercase(str):
+    for intexer in str:
+        temp = intexer
+        if ord(temp) >= 97 and ord(temp) <= 122:
+            temp = chr(ord(intexer) - 32)
+        print("{}".format(temp), end='')
+        print()
