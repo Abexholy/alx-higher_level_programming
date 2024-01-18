6170bf142fa6c74fd9067381a1722bdbc26e9c36
Abexholy <abexholy123@gmail.com>
2024-01-18 06:16:47 -0800

    python

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     if a < 1:
         print("{} arguments.".format(a))
     elif a == 1:
-        print("{} argument".format(a))
+        print("{} argument:".format(a))
     else:
         print("{} arguments:".format(a))
     for i in range(a):
