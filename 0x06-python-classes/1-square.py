7c55cf877a753b42388226d20b59792cc53d5a59
Abexholy <abexholy123@gmail.com>
2024-02-06 10:56:51 -0800

    python

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-
+# 1-square.py
 """Square module."""
 
 class Square:
     """Defines a square."""
 
     def __init__(self, size):
-        """Constructor.
-    Args:
-        size: The length of side in the new square.
+        """Constructor initialize a Square.
+        Args:
+            size: The length of side in the new square.
         """
         self.__size = size
