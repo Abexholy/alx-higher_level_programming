1fd15e03128482bfa017bde85b65cdda1a781981
Abexholy <abexholy123@gmail.com>
2024-02-07 01:32:00 -0800

    code

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    dig = 0
+    dug = 0
     try:
         for item in range(x):
             print("{}".format(my_list[item]), end='')
-            dig += 1
+            dug += 1
     except IndexError:
         pass
     print()
-    return dig
+    return dug
