273c27e411e775128a8c6acc9904496607c95eb1
Abexholy <abexholy123@gmail.com>
2024-02-13 03:02:27 -0800

    python

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# 101-locked_class.py
+# Abiodun Israel
+"""Defines a locked class."""
+
+
+class LockedClass:
+    """This stop instantiations of new class"""
+
+    __slots__ = ["first_name"]
